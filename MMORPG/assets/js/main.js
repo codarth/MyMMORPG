d1d70ea0619312051cbb792a127b096fa097362d
codarth <codarth@users.noreply.github.com>
2020-09-22 21:51:04 -0600

    Collision and sound

--- MMORPG/assets/js/main.js
+++ MMORPG/assets/js/main.js
@@ -24,20 +24,31 @@ function preload() {
     this.load.image('button1', 'assets/images/ui/blue_button01.png');
     this.load.spritesheet('items', 'assets/images/items.png', { frameWidth: 32, frameHeight: 32});
     this.load.spritesheet('characters', 'assets/images/characters.png', { frameWidth: 32, frameHeight: 32});
+    this.load.audio('goldSound', ['assets/audio/Pickup.wav']);
 }
 
 function create() {
+    var goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 0.2});
+
     var button = this.add.image(100, 100, 'button1'); // No animation on image
     button.setOrigin(0.5, 0.5);
 
     this.add.sprite(300,100,'button1'); // Animation available on sprite
 
-    this.add.image(300, 300, 'items', 4);
+    this.chest = this.physics.add.image(300, 300, 'items', 0);
     
-    this.physics.add.image(500, 100, 'button1');
+    this.wall = this.physics.add.image(500, 100, 'button1');
+    this.wall.setImmovable();
 
     this.player = this.physics.add.image(32, 32, 'characters', 0);
     this.player.setScale(2);
+    this.player.body.setCollideWorldBounds(true);
+
+    this.physics.add.collider(this.player, this.wall);
+    this.physics.add.overlap(this.player, this.chest, function(player, chest) {
+        goldPickupAudio.play();
+        chest.destroy();
+    });
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -45,7 +56,7 @@ function create() {
 
 function update() {
     this.player.setVelocity(0);
-    
+
     if(this.cursors.left.isDown){
        this.player.setVelocityX(-160);
     } else if(this.cursors.right.isDown){
