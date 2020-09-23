551312211068c30b02ca790615906d9bd02da83a
codarth <codarth@users.noreply.github.com>
2020-09-22 22:22:55 -0600

    Scenes and classes

---
+++ MMORPG/assets/js/classes/Player.js
@@ -0,0 +1,34 @@
+class Player extends Phaser.Physics.Arcade.Image {
+    constructor(scene, x, y, key, frame){
+        super(scene, x, y, key, frame);
+        this.scene = scene;
+        this.velocity = 160;
+
+        // Enable Physics
+        this.scene.physics.world.enable(this);
+        // Set immovable if another object collides with player
+        this.setImmovable(false);
+        // Scale player
+        this.setScale(2);
+        // Collide with world bounds
+        this.setCollideWorldBounds(true);
+        // Add player to existing scene
+        this.scene.add.existing(this);
+    }
+
+    update(cursors){
+        this.body.setVelocity(0);
+    
+        if(cursors.left.isDown){
+           this.body.setVelocityX(-this.velocity);
+        } else if(cursors.right.isDown){
+            this.body.setVelocityX(this.velocity);
+        }
+    
+        if(cursors.up.isDown){
+            this.body.setVelocityY(-this.velocity);
+        } else if(cursors.down.isDown){
+            this.body.setVelocityY(this.velocity);
+        }
+    }
+}
---
+++ MMORPG/assets/js/scenes/GameScene.js
@@ -0,0 +1,34 @@
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super('Game');        
+    }
+
+    create(){
+        var goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 0.2});
+
+        var button = this.add.image(100, 100, 'button1'); // No animation on image
+        button.setOrigin(0.5, 0.5);
+    
+        this.add.sprite(300,100,'button1'); // Animation available on sprite
+    
+        this.chest = new Chest(this, 300, 300, 'items', 0);
+        
+        this.wall = this.physics.add.image(500, 100, 'button1');
+        this.wall.setImmovable();
+    
+        this.player = new Player(this, 32, 32, 'characters', 0);
+    
+        this.physics.add.collider(this.player, this.wall);
+        this.physics.add.overlap(this.player, this.chest, function(player, chest) {
+            goldPickupAudio.play();
+            chest.destroy();    
+        });
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        this.player.update(this.cursors);
+    }
+
+}
