7e8b2de94f20cf3e750072f40e96aabcdfb098fb
codarth <codarth@users.noreply.github.com>
2020-09-24 21:32:08 -0600

    Spawn chests

--- MMORPG/assets/js/GameManager/ChestModel.js
+++ MMORPG/assets/js/GameManager/ChestModel.js
@@ -1,6 +1,10 @@
 class ChestModel{
-    constructor(){
-        
+    constructor(x, y, gold, spawnerId){
+        this.id = `${spawnerId}-${uuid.v4()}`;
+        this.spawnerId = spawnerId;
+        this.x = x;
+        this.y = y;
+        this.gold = gold;
     }
 
 }
