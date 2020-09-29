43a3ae262ae5e1cb092eb165a906a48019381bd6
codarth <codarth@users.noreply.github.com>
2020-09-28 22:49:16 -0600

    lint and combining game and client folder

---
+++ MMORPG/client/src/scenes/TitleScene.js
@@ -0,0 +1,20 @@
+import * as Phaser from 'phaser';
+import UiButton from '../classes/UiButton';
+
+export default class TitleScene extends Phaser.Scene {
+  constructor() {
+    super('Title');
+  }
+
+  create() {
+    // Create title text
+    this.TitleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'MMORPG', { fontSize: '64px', fill: '#fff' });
+    this.TitleText.setOrigin(0.5);
+
+    this.startGameButton = new UiButton(this, this.scale.width / 2, this.scale.height * 0.65, 'button2', 'button1', 'Start', this.startScene.bind(this, 'Game'));
+  }
+
+  startScene(targetScene) {
+    this.scene.start(targetScene);
+  }
+}
